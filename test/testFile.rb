2c481ce7abcb9d925ecd187f6090ec4b99f10908
mccraig mccraig of the clan mccraig <mccraigmccraig@googlemail.com>
2010-07-14 18:15:11 +0100

    explain File.expand_path test intent

--- test/testFile.rb
+++ test/testFile.rb
@@ -1,9 +1,11 @@
 require 'test/minirunit'
 test_check "Test File:"
 
+# path expansion relative to an in-jar url path with File.join
 test_equal("file:/Users/foo/dev/ruby/jruby/lib/jruby-complete.jar!/META-INF/jruby.home/lib/ruby/site_ruby",
            File.expand_path(File.join( "file:/Users/foo/dev/ruby/jruby/lib/jruby-complete.jar!/META-INF/jruby.home/lib/ruby/site_ruby/1.8", "..")))
 
+# path expansion relative to an in-jar url path with File.expand_path
 test_equal("file:/Users/foo/dev/ruby/jruby/lib/jruby-complete.jar!/META-INF/jruby.home/lib/ruby/site_ruby",
            File.expand_path("..", "file:/Users/foo/dev/ruby/jruby/lib/jruby-complete.jar!/META-INF/jruby.home/lib/ruby/site_ruby/1.8"))
 
